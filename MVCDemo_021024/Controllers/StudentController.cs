f9cac0e661afe85857e9b89791d22f2a60d7cc01
Acer <Acer@DESKTOP-ABKHEEV>
2024-10-02 12:03:00 +0530

    Add project files.

---
+++ MVCDemo_021024/Controllers/StudentController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo_021024.Controllers
+{
+    public class StudentController : Controller
+    {
+        //get
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string userid1,string password1)
+        {
+            return View();
+        }
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View("Index",111);
+        }
+    }
+}
---
+++ MVCDemo_021024/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCDemo_021024.MvcApplication" Language="C#" %>
