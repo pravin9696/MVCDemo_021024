212c3dcf04cf0333b158998c0a3848c295b7af3a
Acer <Acer@DESKTOP-ABKHEEV>
2024-10-04 17:39:15 +0530

    Send data from View to Controller completed

--- MVCDemo_021024/Controllers/StudentController.cs
+++ MVCDemo_021024/Controllers/StudentController.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using MVCDemo_021024.Models;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,6 +12,61 @@ namespace MVCDemo_021024.Controllers
 {
     public class StudentController : Controller
     {
+      
+        public ActionResult NewLogin()
+        {
+           
+            return View();
+
+        }
+        [HttpPost]
+        public ActionResult NewLogin(MyLoginClass obj)
+        {
+
+            return View();
+
+        }
+        [HttpPost]
+        public ActionResult LoginByFormCollection(FormCollection frmc)
+        {
+            string uid = frmc["userid1"];
+            string pass = frmc["password1"];
+            return View();
+        }
+        public ActionResult LoginByRequest()
+        {
+            return View("Login");
+        }
+        [HttpPost]
+        public ActionResult LoginByRequest1()
+        {
+            string userName = Request["userid1"];
+            string password = Request["password1"];
+            //ADO.net code and  check user id and password
+            //1
+            SqlConnection con = new SqlConnection("Data Source=Desktop-ABKHEEV;Initial Catalog=tempdb;Integrated Security=True;Encrypt=True;TrustServerCertificate=True");
+            
+            //2
+            SqlDataAdapter adp = new SqlDataAdapter($"select * from tbllogin where userID='{userName}' AND password1='{password}' ", con);
+            SqlCommandBuilder cmdb = new SqlCommandBuilder(adp);
+
+            //3
+            DataSet ds = new DataSet();
+
+
+            adp.Fill(ds, "tbllogin");
+
+            if (ds.Tables["tbllogin"].Rows.Count>0)
+            {
+              
+            }
+            else
+            {
+
+            }
+
+            return View("Login");
+        }
         //get
         public ActionResult Login()
         {
---
+++ MVCDemo_021024/Models/MyLoginClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCDemo_021024.Models
+{
+    public class MyLoginClass
+    {
+        public string userName { get; set; }
+        public string password { get; set; }
+        public string abc { get; set; }
+    }
+}
